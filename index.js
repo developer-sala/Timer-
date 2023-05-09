d32c65275b64ba71d2ef7e674b36714d3438cb03
sala <113747290+developer-sala@users.noreply.github.com>
2023-05-09 11:47:03 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// 버튼
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset-btn');
+
+// 시간
+let hours = 0;
+let minutes = 0;
+let seconds = 10;
+let timer;
